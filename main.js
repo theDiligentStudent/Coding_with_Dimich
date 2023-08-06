d7d687df41ed7cd35ef65d720781c0ba9b625e8a
theDiligentStudent <ivanrenkin14@gmail.com>
2023-08-06 22:37:46 +0300

    В файле start.js делаю ОДНУ Ф.-ю Вместо Двух, поиск для Каждого Об-а (Кнопки) делаю по id, Убираю Вторую Ф.-ю для События - Ивент, Убираю Второй addEventListener, Убираю Вторую Ф.-Исполнитель для Второго Листенера/Кнопки. В файле main.js создаю Два Об.-а (buttonStart1 и buttonStart2) - на основе Кор.-ра new Start() -- для Каждой из Двух Кнопок Start -- -- вот таким вот Образом -- buttonStart1.start('button_car_1') -- вызываю Об.-ы -- по их id - к жизни.
    Всё корректно работает. К. полностью Работоспособен.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Начинаю заниматься по Новому JS-курсу Димыча "Переход на ООП"
-// 31/07/23
+// 31/07/23      4/08/23     5/08/23
 
-
-
-let buttonStart = new Start;
-buttonStart.start();
+let buttonStart1 = new Start();
+buttonStart1.start('button_car_1');
+let buttonStart2 = new Start();
+buttonStart2.start('button_car_2');
--- start.js
+++ start.js
@@ -1,39 +1,39 @@
 // Начинаю заниматься по Новому JS-курсу Димыча "Переход на ООП"
-// 31/07/23
+// 31/07/23          4/08/23         5/08/23
 
 function Start() {
   this.buttonStart = null;
 
-  this.start = function () {
-
+  this.start = function (id) {
     let buttonsClick = this;
 
-    this.buttonStart1 = document.querySelector('#button_car_1');
-    this.buttonStart2 = document.querySelector('#button_car_2');
+    // Делаю ОДИНАКОВЫЙ поиск для Двух разных Об.-в *Кнопок* по id из html-документа
+    let selectElement = '#' + id;
 
+    // прописываю для Эл.-а "Кнопка" - id
+    this.buttonStart = document.querySelector(selectElement);
+    // this.buttonStart2 = document.querySelector('#button_car_2');
 
-    function onButton1Click(event) {
-        buttonsClick.onButtonStart1Click(event);
+    function onButtonClick(event) {
+      buttonsClick.onButtonStartClick(event);
     }
-    function onButton2Click(event) {
-        buttonsClick.onButtonStart2Click(event);
-    }
+    // function onButton2Click(event) {
+    //     buttonsClick.onButtonStart2Click(event);
+    // }
 
-    this.buttonStart1.addEventListener('click', onButton1Click);
-    this.buttonStart2.addEventListener('click', onButton2Click);
+    this.buttonStart.addEventListener('click', onButtonClick);
+    // this.buttonStart2.addEventListener('click', onButton2Click);
   };
 
-  this.onButtonStart1Click = function () {
+  this.onButtonStartClick = function () {
     window.alert('Hi');
-  }
-  this.onButtonStart2Click = function () {
-    window.alert('Hello');
-  }
-  
+  };
+  // this.onButtonStart2Click = function () {
+  //   window.alert('Hello');
+  // }
 }
 
 // let buttonStart1 = document.querySelectorAll('.buttons');    НЕ рабочий К.
-
 
 // let buttonStart1 = document.querySelector('.buttons');
 
