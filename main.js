bdd1c854f70983145caa990cf6865d18e411c987
theDiligentStudent <ivanrenkin14@gmail.com>
2023-08-06 22:48:09 +0300

    НЕпонятка

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Начинаю заниматься по Новому JS-курсу Димыча "Переход на ООП"
 // 31/07/23      4/08/23     5/08/23
 
+
 let buttonStart1 = new Start();
 buttonStart1.start('button_car_1');
 let buttonStart2 = new Start();
