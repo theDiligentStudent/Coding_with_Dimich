f1a35e7545d02d7b7fb1a73fa372d98a2ce5d3ce
theDiligentStudent <ivanrenkin14@gmail.com>
2023-08-10 17:44:59 +0300

    Немного Переделал Ф.-ю, Car.prototype.onButtonStartClick = function () {... -- так чтобы с Ф.-ей       setTimeout(carCrashed, 5000); -- имитировать Внезапную Остановку Двигателя машины (через 5 сек.)
    и удалил комментарии внизу этого файла

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Начинаю заниматься по Новому JS-курсу Димыча "Переход на ООП"
 // 31/07/23          4/08/23         5/08/23      7/08/23
-    //  8/08/23
+    //  8/08/23       10/08/23
 
 function Car() {
   this.buttonStart = null;
@@ -19,12 +19,22 @@ Car.prototype.start = function (id) {
 
   this.buttonStart.addEventListener('click', onButtonClick);
 };
+ 
+
+  // Модернизирую Ф.-ю для имитации в какой-то момент времени -- остановки Машины -- из-за остановки её Двигателя
 
 Car.prototype.onButtonStartClick = function () {
   // window.alert('Hi');
   let fiftyFifty = Math.random();
   if(fiftyFifty > 0.5) {
     alert('The car started up. The car drove off.');
+    alert('waiting for an accident');
+    function carCrashed() {
+      alert('sudden engine stop');
+      }
+        //  Две строчки Ниже -- две Равнозначные команды     поэтому одну из них я  закомментировал
+      // window.setTimeout(carCrashed, 5000);
+      setTimeout(carCrashed, 5000);
   } else {
     alert('Something went wrong.');
   }
@@ -34,10 +44,4 @@ Car.prototype.onButtonStartClick = function () {
 
 
 
-        // Так - я попробовал сперва Сделать Сам то, что делает Димыч (с Машинами)
-        //     НО надо было делать Совсем По-Другому!!
-// function innerHtml(evenObject) {
-//   let getCurrentTarget = evenObject.currentTarget;
-//   let getInner = getCurrentTarget.innerHtml;
-//   tapStart(getInner);
-// }
+ 
