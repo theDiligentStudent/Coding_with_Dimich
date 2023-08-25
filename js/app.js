24004a21bff0734214717e6c94b5788ae354c732
theDiligentStudent <ivanrenkin14@gmail.com>
2023-08-25 17:04:09 +0300

    Сегодня - 25/08/23. раньше - Что-то делал в этой Папке leson_1, какие-то изменения, - уже и НЕ помню какие. Зафиксирую на Облаке, просто чтобы сохранить. (Естькакая-то НЕ работающая Ф.)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // Начинаю заниматься по Новому JS-курсу Димыча "Переход на ООП"
 // 31/07/23          4/08/23         5/08/23      7/08/23
-    //  8/08/23       10/08/23
+//  8/08/23       10/08/23
 
 function Car() {
   this.buttonStart = null;
@@ -19,31 +19,38 @@ Car.prototype.start = function (id) {
 
   this.buttonStart.addEventListener('click', onButtonClick);
 };
- 
 
-  // Модернизирую Ф.-ю для имитации в какой-то момент времени -- остановки Машины -- из-за остановки её Двигателя
+// Модернизирую Ф.-ю для имитации в какой-то момент времени -- остановки Машины -- из-за остановки её Двигателя
 
 Car.prototype.onButtonStartClick = function () {
-  // window.alert('Hi');
   let fiftyFifty = Math.random();
-  if(fiftyFifty > 0.5) {
+  if (fiftyFifty > 0.5) {
     // Теперь -- так, как учит Димыч -- меняю все alert-ы на console.log
     //   т.к. Димыч говорит, что Программеры всегда в таких случаях используют только console.log
     console.log('The car started up. The car drove off.');
-    console.log('waiting for an accident');
+
+    this.buttonStart.classList.add('hide');
+    
+// this.buttonStart.disabled = true;    
+// Так, как было в курсе Димыча "ваще с нуля" --- здесь уже этот способ НЕ работает......
+
     function carCrashed() {
       console.log('sudden engine stop');
-      }
-        //  Две строчки Ниже -- две Равнозначные команды     поэтому одну из них я  закомментировал
-      // window.setTimeout(carCrashed, 5000);
-      setTimeout(carCrashed, 5000);
+      this.buttonStart.classList.remove('hide');
+
+
+      // this.buttonStart.disabled = false;  
+    }
+    //  Две строчки Ниже -- две Равнозначные команды     поэтому одну из них я  закомментировал
+    // window.setTimeout(carCrashed, 5000);
+    setTimeout(carCrashed, 5000);
+
+// this.buttonStart.disabled = false;     Так, как было в курсе Димыча "ваще с нуля" --- здесь уже этот способ НЕ работает......
+
+    // Переместил строчку К.-а ниже из положения Перед Ф.-ей setTimeout, поставил её Позади setTimeout.
+    console.log('waiting for an accident');
+
   } else {
     console.log('Something went wrong.');
   }
 };
-
-
-
-
-
- 
